dce58620e97ffb23b5c500dc11fd64e657ce785f
khandesaiprakash <120784159+khandesaiprakash@users.noreply.github.com>
2023-03-29 21:09:33 +0530

    README update

---
+++ Findthelargestnumber.java
@@ -0,0 +1,32 @@
+//Create the import statement, it works on  a users input!
+import java.util.Scanner;
+public class Findthelargestnumber {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new Scanner(System.in);
+
+        //print the first number
+        System.out.println("Enter the first number:");
+        int firstNumber = sc.nextInt();    //inetegr input method
+        //print the second number
+        System.out.println("Enter the second element:");
+        int secondNumber = sc.nextInt();
+        //print the third element
+        System.out.println("Enter the third element:");
+        int thirdNumber = sc.nextInt();
+        //maximum number
+        int maxNumber = 0;
+        //First condition
+        if(firstNumber > maxNumber){
+            maxNumber = firstNumber;
+        }
+        if(secondNumber > maxNumber) {
+            maxNumber = secondNumber;
+        }
+        if(thirdNumber > maxNumber) {
+            maxNumber = thirdNumber;
+        }
+        // print the result
+        System.out.println("The highest number is: " + maxNumber);
+
+    }
+}
